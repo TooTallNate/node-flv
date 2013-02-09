d4323875092179ebf27b7405e4713787b31e350b
Nathan Rajlich <nathan@tootallnate.net>
2013-02-08 18:48:47 -0800

    initial commit
    Only a partially implemented Decoder class so far...

---
+++ index.js
@@ -0,0 +1,7 @@
+
+/**
+ * The `Decoder` class accepts an FLV file written to it and splits it apart into
+ * the meaningful "audio" and "video" substreams.
+ */
+
+exports.Decoder = require('./lib/decoder');
